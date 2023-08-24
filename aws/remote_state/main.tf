f23f1a76a361c414f7a8778f1137ee46e913cc2a
sriikarkrishna <141422017+sriikarkrishna@users.noreply.github.com>
2023-08-24 17:15:05 +0530

    main.tf

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -11,15 +11,16 @@
 terraform {
   required_version = ">= 0.12"
 }
-backend "s3" {
-    bucket         = "terraform_state"
+  backend "s3" {
+    bucket         = "THE_NAME_OF_THE_STATE_BUCKET"
     key            = "some_environment/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     kms_key_id     = "THE_ID_OF_THE_KMS_KEY"
-    dynamodb_table = "terraform_lock"
+    dynamodb_table = "THE_ID_OF_THE_DYNAMODB_TABLE"
   }
 }
+
 # ------------------------------------------------------------------------------
 # CONFIGURE OUR AWS CONNECTION
 # ------------------------------------------------------------------------------
